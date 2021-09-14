dc3de8c2b44a2e7dbc9f52b2925ee98993069d16
Valdeci Gomes <valfilho@amazon.com>
2021-09-13 22:58:49 -0300

    Change Car constructor

--- src/main/kotlin/ci/valde/Car.kt
+++ src/main/kotlin/ci/valde/Car.kt
@@ -2,4 +2,6 @@ package ci.valde
 
 import org.bson.types.ObjectId
 
-data class Car(var id: ObjectId?, var model: String, var year: Int)
+data class Car(var id: ObjectId?, var model: String, var year: Int){
+    constructor(): this(null, "not_set", 1970)
+}
