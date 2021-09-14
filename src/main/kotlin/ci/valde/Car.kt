16c26bf9aeab604785efc1e74f6b14632d4d2c91
Valdeci Gomes <valfilho@amazon.com>
2021-09-13 22:43:19 -0300

    Remove Car class constructor

--- src/main/kotlin/ci/valde/Car.kt
+++ src/main/kotlin/ci/valde/Car.kt
@@ -2,6 +2,4 @@ package ci.valde
 
 import org.bson.types.ObjectId
 
-data class Car(var id: ObjectId?, var model: String, var year: Int){
-    constructor(): this(null, "not_set", 1970)
-}
+data class Car(var id: ObjectId?, var model: String, var year: Int)
